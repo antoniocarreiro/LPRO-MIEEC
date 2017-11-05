2ca12a537a0977dc894bf58435433c1157b34e1d
antoniocarreiro <up201404828@fe.up.pt>
2017-11-05 15:30:17 +0000

    Added Unit tests a) and b)

---
+++ SQ-2/src/dkeep/logic/PositionTest.java
@@ -0,0 +1,82 @@
+package dkeep.logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PositionTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testHeroToFreeCell() {
+		DragonsBane game = new DragonsBane(0);
+		assertEquals(1, game.hero.getX());
+		assertEquals(1, game.hero.getY());
+		game.newTurn("s");
+		assertEquals(2, game.hero.getX());
+		assertEquals(1, game.hero.getY());
+		game.newTurn("w");
+		assertEquals(1, game.hero.getX());
+		assertEquals(1, game.hero.getY());
+		game.newTurn("d");
+		assertEquals(1, game.hero.getX());
+		assertEquals(2, game.hero.getY());
+		game.newTurn("a");
+		assertEquals(1, game.hero.getX());
+		assertEquals(1, game.hero.getY());
+	}
+
+	@Test
+	public void testHeroToWall() {
+		DragonsBane game = new DragonsBane(0);
+		assertEquals(1, game.hero.getX());
+		assertEquals(1, game.hero.getY());
+		game.newTurn("w");
+		assertEquals(1, game.hero.getX());
+		assertEquals(1, game.hero.getY());
+		game.newTurn("a");
+		assertEquals(1, game.hero.getX());
+		assertEquals(1, game.hero.getY());
+		game.newTurn("s");
+		assertEquals(2, game.hero.getX());
+		assertEquals(1, game.hero.getY());
+		game.newTurn("d");
+		assertEquals(2, game.hero.getX());
+		assertEquals(1, game.hero.getY());
+		game.newTurn("s");
+		assertEquals(3, game.hero.getX());
+		assertEquals(1, game.hero.getY());
+		game.newTurn("s");
+		assertEquals(4, game.hero.getX());
+		assertEquals(1, game.hero.getY());
+		game.newTurn("s");
+		assertEquals(5, game.hero.getX());
+		assertEquals(1, game.hero.getY());
+		game.newTurn("d");
+		assertEquals(5, game.hero.getX());
+		assertEquals(2, game.hero.getY());
+		game.newTurn("s");
+		assertEquals(5, game.hero.getX());
+		assertEquals(2, game.hero.getY());
+	}
+
+}
